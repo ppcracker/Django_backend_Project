df5956d7e6008297d3d84f9848cbca2e5c6375d1
Deepika <146109841+ppcracker@users.noreply.github.com>
2025-06-18 19:13:48 +0530

    Create models.py

---
+++ Core/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+class TelegramUser(models.Model):
+    username = models.CharField(max_length=255, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
